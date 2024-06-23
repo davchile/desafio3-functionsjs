78ceaa9dc7b718102358e37b0c2652b419cb51a9
David Paillacheo P <david.paillacheo@ticonsultores.cl>
2024-06-23 01:19:24 -0400

    ready2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,9 @@
 //Ejercicio 2.1
-// function pintar() {
-//     ele.style.backgroundColor = 'yellow'
-// }
-
-// const ele = document.querySelector("#ele1")
-// ele.addEventListener("click", pintar)
+function pintar() {
+    ele.style.backgroundColor = 'yellow'
+}
+const ele = document.querySelector("#ele1")
+ele.addEventListener("click", pintar)
 
 //Función anónima 2.2
 // const element = document.querySelector("#ele1")
@@ -13,12 +12,12 @@
 // })
 
 //Función anónima 2.3
-function cambiarColor(color = "green") {
-    const element = document.querySelector("#ele1")
-    element.style.backgroundColor = color
-}
-const elementUno = document.querySelector("#ele1")
-elementUno.addEventListener("click", ()=> {
-    cambiarColor("yellow")
-})
+// function cambiarColor(color = "green") {
+//     const element = document.querySelector("#ele1")
+//     element.style.backgroundColor = color
+// }
+// const elementUno = document.querySelector("#ele1")
+// elementUno.addEventListener("click", ()=> {
+//     cambiarColor("yellow")
+// })
 //**Por favor descomentar cada ejercicio para evaluar funcionalidad */
